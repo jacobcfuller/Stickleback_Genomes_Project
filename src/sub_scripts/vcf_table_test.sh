eba4d8a6f320ce01bbbdda06347ae6627c1c69fb
jcfuller <jacobcfuller93@gmail.com>
2016-10-25 16:38:07 -0400

    fix multiline commands

--- src/sub_scripts/vcf_table_test.sh
+++ src/sub_scripts/vcf_table_test.sh
@@ -18,16 +18,16 @@ module load picard/2.4.1
 
 #first
 
-java -jar /usr/local/apps/gatk/3.4.0/GenomeAnalysisTK.jar
--T SelectVariants
--R /lustre1/jcfuller/Stickleback_Genomes_Project/doc/ref/revisedAssemblyUnmasked.fa
--V /lustre1/jcfuller/Stickleback_Genomes_Project/data/genome/bam/PaxtonBenthicAlignment/paxton_JG.vcf_table_test
--o single_sample_test.vcf
+java -jar /usr/local/apps/gatk/3.4.0/GenomeAnalysisTK.jar \
+-T SelectVariants \
+-R /lustre1/jcfuller/Stickleback_Genomes_Project/doc/ref/revisedAssemblyUnmasked.fa \
+-V /lustre1/jcfuller/Stickleback_Genomes_Project/data/genome/bam/PaxtonBenthicAlignment/paxton_JG.vcf_table_test \
+-o single_sample_test.vcf \
 -sn male9
 
-java -jar /usr/local/apps/gatk/3.4.0/GenomeAnalysisTK.jar
--T VariantsToTable
--R /lustre1/jcfuller/Stickleback_Genomes_Project/doc/ref/revisedAssemblyUnmasked.fa
--V single_sample_test.vcf
--F POS -GF GT -GF PL -GF GQ 
--o single_sample_test.table
+java -jar /usr/local/apps/gatk/3.4.0/GenomeAnalysisTK.jar \
+-T VariantsToTable \
+-R /lustre1/jcfuller/Stickleback_Genomes_Project/doc/ref/revisedAssemblyUnmasked.fa \
+-V single_sample_test.vcf \
+-F POS -GF GT -GF PL -GF GQ  \
+-o single_sample_test.table 
