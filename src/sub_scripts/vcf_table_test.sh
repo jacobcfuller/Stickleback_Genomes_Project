3c4627c9e0ed9abc31c6181fe1c8c8023f0dc434
jcfuller <jacobcfuller93@gmail.com>
2016-10-25 16:36:08 -0400

    more sapelo adjustments...always

--- src/sub_scripts/vcf_table_test.sh
+++ src/sub_scripts/vcf_table_test.sh
@@ -4,7 +4,6 @@
 #PBS -l nodes=1:ppn=8
 #PBS -l mem=20
 #PBS -l walltime=96:00:00
-#PBS -M jacobcfuller93@gmail.com
 #PBS -m ae
 #PBS -j oe
 
@@ -14,21 +13,21 @@
 cd /lustre1/jcfuller/Stickleback_Genomes_Project/data/genome/bam/PaxtonBenthicAlignment/
 
 module load java/jdk1.8.0_20
-module load picard/2.4.1 
+module load picard/2.4.1
 
 
 #first
 
-java -jar /usr/local/apps/gatk/3.4.0/GenomeAnalysisTK.jar \\
--T SelectVariants \\
+java -jar /usr/local/apps/gatk/3.4.0/GenomeAnalysisTK.jar
+-T SelectVariants
 -R /lustre1/jcfuller/Stickleback_Genomes_Project/doc/ref/revisedAssemblyUnmasked.fa
 -V /lustre1/jcfuller/Stickleback_Genomes_Project/data/genome/bam/PaxtonBenthicAlignment/paxton_JG.vcf_table_test
 -o single_sample_test.vcf
 -sn male9
 
-java -jar /usr/local/apps/gatk/3.4.0/GenomeAnalysisTK.jar \\
--T VariantsToTable \\
--R /lustre1/jcfuller/Stickleback_Genomes_Project/doc/ref/revisedAssemblyUnmasked.fa \\
--V single_sample_test.vcf \\
--F POS -GF GT -GF PL -GF GQ \\
+java -jar /usr/local/apps/gatk/3.4.0/GenomeAnalysisTK.jar
+-T VariantsToTable
+-R /lustre1/jcfuller/Stickleback_Genomes_Project/doc/ref/revisedAssemblyUnmasked.fa
+-V single_sample_test.vcf
+-F POS -GF GT -GF PL -GF GQ 
 -o single_sample_test.table
