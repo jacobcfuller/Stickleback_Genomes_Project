984b27c146013294d5b4742b059563f31a56bc09
jcfuller <jacobcfuller93@gmail.com>
2016-10-31 17:02:48 -0400

    added sliding window script

--- src/slidingWindow/slidingWindow.py
+++ src/slidingWindow/slidingWindow.py
@@ -1,5 +1,6 @@
 #!/home/jcfuller/anaconda3/bin/python3.5
 
+import pandas as pd
 import numpy as np
 import subprocess
 import argparse
@@ -10,8 +11,8 @@ ScriptDescript = '''Sliding Window function on SNP calls'''
 Parser = argparse.ArgumentParser(description=ScriptDescript)
 Parser.add_argument('-w', '--window', type=int, metavar='W', required=False, default=500,
                     help='Length of the sliding window')
-Parser.add_argument('-i', '--increment', type=int, metavar='I', required=False, default=250,
-                    help='Increment by which to slide window')
+#Parser.add_argument('-i', '--increment', type=int, metavar='I', required=False, default=250,
+#                    help='Increment by which to slide window')
 Parser.add_argument('file', type=str, metavar='F')
 
 # Read args
@@ -19,7 +20,7 @@ args = vars(Parser.parse_args())
 
 # make objects from args
 window = args['window']
-incr = args['increment']
+#incr = args['increment']
 file = args['file']
 del args
 
@@ -36,8 +37,16 @@ for i in range(len(vcfVarList)):
     varLocation = vcfVarList[i]
     varPresent[varLocation-1] = 1
 
+numWin = int(chrXIX / window)
 
+slidWinCount = np.arange(numWin)
 
+for x in range(numWin):
+    counter=0
+    for i in range(500):
+        if(varPresent[i+(500*x)] == 1):
+            counter += 1
+    slidWinCount[x] = counter
 
 
 
