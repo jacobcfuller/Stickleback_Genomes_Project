e0699d58086e288bc88e539bdc8dae2ad05d3a86
jcfuller <jacobcfuller93@gmail.com>
2016-10-31 13:21:36 -0400

    set up parser

--- src/slidingWindow/slidingWindow.py
+++ src/slidingWindow/slidingWindow.py
@@ -5,6 +5,7 @@ import subprocess
 import argparse
 
 
+# Set up parser
 ScriptDescript = '''Sliding Window function on SNP calls'''
 Parser = argparse.ArgumentParser(description=ScriptDescript)
 Parser.add_argument('-w', '--window', type=int, metavar='W', required=False, default=500,
@@ -13,11 +14,15 @@ Parser.add_argument('-i', '--increment', type=int, metavar='I', required=False, 
                     help='Increment by which to slide window')
 Parser.add_argument('file', type=str, metavar='F')
 
+# Read args
 args = vars(Parser.parse_args())
 
+# make objects from args
 window = args['window']
 incr = args['increment']
 file = args['file']
 del args
+
+
 
 
