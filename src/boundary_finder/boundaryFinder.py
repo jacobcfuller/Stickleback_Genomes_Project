5ed08e25f2153e2b165c740a215bfba36f68f3ff
jcfuller <jacobcfuller93@gmail.com>
2016-11-08 15:56:56 -0500

    compensate for coverage

--- src/boundary_finder/boundaryFinder.py
+++ src/boundary_finder/boundaryFinder.py
@@ -12,26 +12,39 @@ chrXIX = 20612724
 # ======================== #
 
 
-def logSlidingWindow(logDF):
-    '''Get sliding average of log values for file subset.'''
-    slidingMeans = logDF.rolling(100).mean
-    return slidingMeans
+# def locationFinder(logDF):
 
 
 def getSubSet(file, rowsToSkip, footerToSkip):
     '''Return the relevant subset of the table to be analyzed.
        Header set to 0.
     '''
-    fileSubSet = pandas.read_table(file, engine='python', skiprows=rowsToSkip,
-                                   skipfooter=footerToSkip, header=None)
+    fileSubSet = pandas.read_table(
+                                   file,
+                                   engine='python',
+                                   skiprows=rowsToSkip,
+                                   skipfooter=footerToSkip,
+                                   header=None
+                                                     )
     return fileSubSet
 
 
 def takeMaleFemaleLog(male, female):
     '''Takes male and female DataFrames and produces a new log2(male/female)
        DataFrame, setting log2(0/0) to nan.
+       Compensates for coverage difference.
     '''
-    logDF = pandas.DataFrame(index=np.arange(len(male)), columns=['log(male/female)'])
+    maleSum = male.sum()
+    femaleSum = female.sum()
+    if(maleSum[0] > femaleSum[0]):
+        female = female*(maleSum[0]/femaleSum[0])
+    else:
+        male = male*(femaleSum[0]/maleSum[0])
+
+    logDF = pandas.DataFrame(
+                             index=np.arange(len(male)),
+                             columns=['log(male/female)']
+                                                         )
     for x in range(len(male)):
         if male.iloc[x, 0] != 0 and female.iloc[x, 0] != 0:
             logX = math.log2(male.iloc[x, 0]/female.iloc[x, 0])
@@ -70,10 +83,18 @@ def getSkipFooter(inputIncr):
 # ======================== #
 
 if __name__ == '__main__':
-    rowToSkip = getSkipRows(250)
+    rowsToSkip = getSkipRows(250)
     footerToSkip = getSkipFooter(250)
 
-    maleSubset = getSubSet("Win_1000_incr_250_POM544_XIX_depth.txt", rowToSkip,
-                           footerToSkip)
-    femaleSubset = getSubSet("Win_1000_incr_250_POF_543_XIX.txt", rowToSkip,
-                             footerToSkip)
+    male = getSubSet(
+                     "Win_1000_incr_250_POM544_XIX_depth.txt",
+                     rowsToSkip,
+                     footerToSkip
+                                 )
+    female = getSubSet(
+                       "Win_1000_incr_250_POF_543_XIX.txt",
+                       rowsToSkip,
+                       footerToSkip
+                                   )
+    logDF = takeMaleFemaleLog(male, female)
+    print(logDF)
