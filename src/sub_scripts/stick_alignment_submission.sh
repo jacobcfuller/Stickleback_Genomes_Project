db3492d03d1a1f5e767b0a493cd5f18a91d549d0
jcfuller <jacobcfuller93@gmail.com>
2016-11-16 11:11:53 -0500

    adjust scripts for zcluster - sapelo down

--- src/sub_scripts/stick_alignment_submission.sh
+++ src/sub_scripts/stick_alignment_submission.sh
@@ -7,23 +7,15 @@ read fetch
 while read -r line
 do
 
-$(cat << EOF > /home/jcfuller/scripts/stick_alignment_individual_sub.sh
-#PBS -S /bin/bash
-#PBS -q batch
-#PBS -N ${line}
-#PBS -l nodes=1:ppn=12:AMD
-#PBS -l mem=2gb
-#PBS -l walltime=96:00:00
-#PBS -M jacobcfuller93@gmail.com
-#PBS -m ae
-#PBS -j oe
+$(cat << EOF > /home/mwlab/jcfuller/stick_alignment_individual_sub.sh
 
-/home/jcfuller/scripts/alignment.sh -s ${line}
+
+/home/mwlab/jcfuller/alignment.sh -s ${line}
 EOF
 )
 
 sleep 1s
-qsub /home/jcfuller/scripts/stick_alignment_individual_sub.sh
+qsub -q rcc-30d /home/mwlab/jcfuller/stick_alignment_individual_sub.sh
 sleep 1s
 
 done < ${fetch}
