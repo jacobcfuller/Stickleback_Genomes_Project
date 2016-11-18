2723253512ba688a82a6c7b31203e3a4fb72d945
jcfuller <jacobcfuller93@gmail.com>
2016-11-18 14:56:23 -0500

    edit functions, add main

--- src/par_boundary/slideWin.py
+++ src/par_boundary/slideWin.py
@@ -1,83 +1,139 @@
 #!/home/jcfuller/anaconda3/bin/python3.5
 
-'''GetChunks gets average for each chunk pulled out.
+'''getChunks gets average for each chunk pulled out.
 slidingWin does rolling average of the above averages.
 
-GetChunks "chunksize" sets the number of points, while SlidingWin adjusts
+getChunks "chunksize" sets the number of points, while slidingWin adjusts
 how smooth each average will be
 THIS IS GENIUS
 
 stolen from Lucas A. Nell
 '''
-import pandas as pd
+import argparse
 import subprocess as sp
+import pandas as pd
+
 
 # ======================== #
 #       Functions          #
 # ======================== #
 
 
-def GetChunks(File):
+def getChunks(File):
     '''Function to read raw depths and create a pd.DataFrame of chunk means.
 
     Outputs pd.DataFrame for next step.
     '''
 
-    RawDepthTab = pd.read_table(
-                                File,
+    rawDepthTab = pd.read_table(File,
                                 compression='gzip',
                                 delim_whitespace=True,
                                 chunksize=5000)
 
-    ChunkAvgs = (chunk.mean() for chunk in RawDepthTab)
+    chunkAvgs = (chunk.mean() for chunk in rawDepthTab)
 
-    Length = int(sp.check_output('gunzip -c %s | wc -l' % File, shell=True).strip()) - 1
+    tableLength = int(sp.check_output('gunzip -c %s | wc -l' % File,
+                                      shell=True)
+                        .strip()) - 1
 
-    return pd.DataFrame(ChunkAvgs), Length
+    return pd.DataFrame(chunkAvgs), tableLength
 
 
-def CleanEnd(RollingChunks, ChunkAvgs, Length):
-    '''Adjusts last rolling-chunk average for uneven last-chunk length.
+def cleanEnd(rollingChunks, chunkAvgs, tableLength):
+    '''Adjusts last rolling-chunk average for uneven last-chunk tableLength.
 
     Args:
-        RollingChunks (pd.DataFrame): Table of rolling chunk averages.
-        ChunkAvgs (pd.DataFrame): Table of chunk-mean averages.
-        Length (int): Length of sequence.
+        rollingChunks (pd.DataFrame): Table of rolling chunk averages.
+        chunkAvgs (pd.DataFrame): Table of chunk-mean averages.
+        tableLength (int): tableLength of sequence.
         ind (int): Index of column you're fixing.
 
     Returns:
         (float): New rolling-chunk average.
     '''
-    Partial = Length % Incr  # Size of last chunk (!=Incr)
-    Avg0 = RollingChunks.iloc[-1, 0]
-    End = ChunkAvgs.iloc[-1, 0]
+    partial = tableLength % incr  # Size of last chunk (!=incr)
+    avg0 = rollingChunks.iloc[-1, 0]
+    end = chunkAvgs.iloc[-1, 0]
     # Sum of all chunk avgs other than end one
-    NonEnd = (Avg0 * chuWin) - End
-    # New sum of chunk averages, where End is underweighted
-    Total = (End * (Partial / Incr)) + NonEnd
-    # New chuWin, where End is underweighted
-    EndN = (chuWin - 1) + (Partial / Incr)
-    return Total / EndN
+    nonEnd = (avg0 * chuWin) - end
+    # New sum of chunk averages, where end is underweighted
+    total = (end * (partial / incr)) + nonEnd
+    # New chuWin, where end is underweighted
+    endN = (chuWin - 1) + (partial / incr)
+    return total / endN
 
 
-def SlidingWin(ChunkAvgDF, Length):
+def slidingWin(chunkAvgDF, tableLength):
     '''Create table of read-depth averages across a sliding window.
 
     Args:
-        ChunkAvgDF (pd.DataFrame): Table of chunk averages.
-        Length (int): Total length of sequence.
+        chunkAvgDF (pd.DataFrame): Table of chunk averages.
+        tableLength (int): Total tableLength of sequence.
 
     Returns:
         Nothing. Writes output to file.
     '''
-    # Calculates a rolling mean of length 'chuWin', then shifts it so it starts
+    # Calculates a rolling mean of tableLength 'chuWin', then shifts it so it starts
     # with numbers, then it removes the NaN at the end
-    RollingChunks = ChunkAvgDF.rolling(window=2).mean().dropna() \
-                              .reset_index(drop=True)
-    RollingChunks.iloc[-1, 0] = CleanEnd(RollingChunks, ChunkAvgDF, Length, 0)
+    rollingChunks = (chunkAvgDF.rolling(window=2)
+                               .mean()
+                               .dropna()
+                               .reset_index(drop=True))
 
-    return RollingChunks
+    rollingChunks.iloc[-1, 0] = cleanEnd(rollingChunks,
+                                         chunkAvgDF,
+                                         tableLength)
+    return rollingChunks
 
-DF, length = GetChunks("/home/jcfuller/Documents/White_lab/Stickleback_Genomes_Project/data/\
-depth_analysis/Paxton_lake/raw_coverage/PB_male10_q_XIX.txt.gz")
-print(DF)
+
+def getSlidwin(depthFile):
+    """Give it a file name, it'll run slidingWin, saving output."""
+    chunkAvgDF, tableLength = getChunks(depthFile)
+    rollingChunks = slidingWin(chunkAvgDF, tableLength)
+    # following outFile line only works if given file argument is the name of file
+    # not the name of the path of the file
+    outFile = 'Win_'+str(window)+'_incr_'+str(incr)+'_' + depthFile
+    rollingChunks.to_csv(outFile,
+                         index=False,
+                         sep='\t',
+                         compression='gzip',
+                         float_format='%-0.5f')
+
+
+# ======================== #
+#           Main           #
+# ======================== #
+
+if __name__ == '__main__':
+
+    ScriptDescript = '''Sliding Window function on raw read depth files.
+     -wd needs to be directory with files
+     '''
+
+    Parser = argparse.ArgumentParser(description=ScriptDescript)
+    Parser.add_argument('-w',
+                        '--window',
+                        type=int,
+                        metavar='W',
+                        required=False,
+                        default=1000,
+                        help='Length of the sliding window')
+    Parser.add_argument('-i',
+                        '--increment',
+                        type=int,
+                        metavar='I',
+                        required=False,
+                        default=500,
+                        help='Increment by which to slide window')
+    Parser.add_argument('file',
+                        type=str,
+                        metavar='F',
+                        help='Raw depth (ending in ".txt.gz") file')
+    args = vars(Parser.parse_args())
+    window = args['window']
+    incr = args['increment']
+    depthFile = args['file']
+    chuWin = int(window/incr)
+    del args  # No longer needed
+
+    getSlidwin(depthFile)
