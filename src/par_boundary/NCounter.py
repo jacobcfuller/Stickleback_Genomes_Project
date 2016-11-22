045884a476ded768608abb9535c663fe62d5a5ea
jcfuller <jacobcfuller93@gmail.com>
2016-11-22 16:51:16 -0500

    modify output & list size for better plotting in par.py

--- src/par_boundary/NCounter.py
+++ src/par_boundary/NCounter.py
@@ -1,10 +1,12 @@
 #!/home/jcfuller/anaconda3/bin/python3.5
 
+import numpy as np
+
 '''Program to count the # of Ns in a fasta file. in a window of 250 bp, if more
 than half are N, this goes as "1" otherwise 0. prints list of 1/0
 '''
 
-chrXIX = open('chrXIX.fa')
+chrXIX = open("/home/jcfuller/Documents/White_lab/Stickleback_Genomes_Project/data/chrXIX.fa")
 
 # header line
 line = chrXIX.readline
@@ -22,11 +24,15 @@ while line:
     if lineCount is 5:
         lineCount = 0
         if NCount > 125:
-            NCountList.append(1)
-        else:
             NCountList.append(0)
+        else:
+            NCountList.append(float('nan'))
         NCount = 0
-        print(NCountList[index])
         index += 1
-
 chrXIX.close()
+
+subset = NCountList[8800:14399]
+
+nplist = np.asarray(subset)
+
+np.savetxt("NCount.txt", nplist)
