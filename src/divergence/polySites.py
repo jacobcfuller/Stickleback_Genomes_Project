7ede381b5dad428332fdd654792bb174c4dbb0c0
jcfuller <jacobcfuller93@gmail.com>
2017-01-19 10:05:38 -0500

    add Parser to main. Comment functions

--- src/divergence/polySites.py
+++ src/divergence/polySites.py
@@ -1,45 +1,60 @@
 #!/home/jcfuller/anaconda3/bin/python3.5
 
-# Must remove 82M19 unorder scaffold before using this
-
+# Must remove 82M19 unordered scaffold before using this
+import argparse
 import pandas as pd
 
 
 def makeDF(snpTable):
+    '''Import Y chromosome SNP call table into panda dataframe.
+       table should have 3 columns in the following order:
+       POS  REF  ALT
+       Only uses POS for now, but retain others for future functionality
+    '''
     snpDF = pd.read_table(snpTable, low_memory=False)
 
     return snpDF
 
 
 # This shit's recursive, y'all
-def fillInZeros(maleDF, index, winCount):
+def fillInZeros(maleDF, index, winCount, incr):
+    '''If the bp location of the next SNP call is greater than window size,
+       fill in with '0' for 0 SNPs in that window. Repeat recursively.
+       Increments winCount and returns the new count for slideWindow.
+    '''
     mDF = maleDF
-    if(mDF.iloc[index+1]['POS'] >= (1000*(winCount))):
-        print((1000*(winCount)), '\t', '0')
+    if(mDF.iloc[index+1]['POS'] >= (incr*winCount)):
+        print((incr*winCount), '\t', '0')
         winCount += 1
-        return fillInZeros(mDF, index, winCount)
+        return fillInZeros(mDF, index, winCount, incr)
     else:
         return winCount
 
 
-def slideWindow(mDF, fDF, incr):
+def slideWindow(maleFile, femaleFile, incr):
+    '''Sliding window count of SNPs that are present in male Y VCF, but not
+       female, assuming that these are unique Y chr polymorphisms.
+       Prints bp position and snp count for that window.
+    '''
+    # file header
+    mDF = makeDF(maleFile)
+    fDF = makeDF(femaleFile)
+    print("pos", '\t', 'snp #')
     snpCount = 0
     winCount = 1
-
     # remember DF is 0-index
     for index in mDF.index:
-        #print(index)
         # If SNP call doesn't exist in female, count
         if((mDF.iloc[index]['POS'] in fDF['POS'].values) is False):
             snpCount += 1
-        # if end of 1000bp window, record stat
-        if(mDF.iloc[index+1]['POS'] >= (1000*winCount)):
-            print((1000*winCount), '\t', snpCount)
+        # need to figure out tail end of dataframe
+        if(mDF.iloc[index+1]['POS'] >= (incr*winCount)):
+            print((incr*winCount), '\t', snpCount)
             # reset counter
             snpCount = 0
             # check if there are actually snps in the next window
             winCount += 1
-            winCount = fillInZeros(mDF, index, winCount)
+            winCount = fillInZeros(mDF, index, winCount, incr)
 
 
 # ======================== #
@@ -48,8 +63,19 @@ def slideWindow(mDF, fDF, incr):
 
 
 if __name__ == '__main__':
-    # tests
-    Fdf = makeDF('/home/jcfuller/Documents/White_lab/Stickleback_Genomes_Project/data/snps/LW_F.table')
-    Mdf = makeDF('/home/jcfuller/Documents/White_lab/Stickleback_Genomes_Project/data/snps/LW_M.table')
-    print("pos", '\t', 'snp #')
-    slideWindow(Mdf, Fdf, 1)
+    ScriptDescript = '''Sliding window SNP count'''
+    Parser = argparse.ArgumentParser(description=ScriptDescript)
+    Parser.add_argument('-m', '--maleFile', type=str, metavar='M',
+                        required=True)
+    Parser.add_argument('-f', '--femaleFile', type=str, metavar='F',
+                        required=True)
+    Parser.add_argument('-i', '--increment', type=float, metavar='I',
+                        required=True)
+    args = vars(Parser.parse_args())
+
+    # Set args to variables
+    maleFile = args['maleFile']
+    femaleFile = args['femaleFile']
+    incr = args['increment']
+    del args
+    slideWindow(maleFile, femaleFile, incr)
