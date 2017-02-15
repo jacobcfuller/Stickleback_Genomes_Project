09d3afff0a1f17e495a4567e9c5f0a6f54f959ea
jcfuller <jacobcfuller93@gmail.com>
2017-02-15 16:00:50 -0500

    N counter for Y chromosome. basicalyl copy of NCounter.py

--- src/divergence/findN.py
+++ src/divergence/findN.py
@@ -1,15 +1,25 @@
 #!/home/jcfuller/anaconda3/bin/python3.5
 
+import numpy as np
+
 fa_file = ("/home/jcfuller/Documents/White_lab/Stickleback_Genomes_Project/doc"
            "/Y_Chr_JoinedAssembly.fa")
 
+N_count_list = []
 pos = 0
+N_count = 0
 with open(fa_file) as fa:
     header = next(fa)
     for line in fa:
-        if ">" in line:
-            break
-        if "N" in line:
-            pos += len(line.split('N')[0])
-            break
-        pos += len(line.strip())
+        for base in line:
+            pos += 1
+            if base is 'N':
+                N_count += 1
+            if (pos % 1000) == 0:
+                if N_count >= 500:
+                    N_count_list.append(0)
+                else:
+                    N_count_list.append(float('nan'))
+                N_count = 0
+
+np.savetxt("Y_N_count.txt", np.asarray(N_count_list))
